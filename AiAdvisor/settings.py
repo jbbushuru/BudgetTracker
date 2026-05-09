b0aca6c8ff0dd9120248c2801addabf069c12f02
Joan Bushuru <jbbushuru@gmail.com>
2026-05-09 07:28:51 +0300

    trial4: os.environ with square brackets[]

--- AiAdvisor/settings.py
+++ AiAdvisor/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'AiAdvisor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':os.environ('MYSQL_DATABASE'),
-        'USER': os.environ('MYSQLUSER'),
-        'PASSWORD': os.environ('MYSQLPASSWORD'),
-        'HOST': os.environ('MYSQLHOST'),
-        'PORT': os.environ('MYSQLPORT'),
+        'NAME':os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQLUSER'],
+        'PASSWORD': os.environ['MYSQLPASSWORD'],
+        'HOST': os.environ['MYSQLHOST'],
+        'PORT': os.environ['MYSQLPORT'],
     }
 }
 
